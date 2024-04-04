6b865e9a5989921eec4aa4a50318965e7433d90d
thisisbayzed <bayzed.dev@gmail.com>
2024-04-04 15:53:12 +0600

    bug fix

--- components/forms/Forms.jsx
+++ components/forms/Forms.jsx
@@ -32,7 +32,7 @@ function Forms({ placeholderText, isComment, postId }) {
     } catch (error) {
       toast.error("Something went wrong");
     }
-  }, [body, mutatePosts, postId, isComment]);
+  }, [body, mutatePosts, postId, isComment , mutateFetchedPost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
@@ -69,7 +69,7 @@ function Forms({ placeholderText, isComment, postId }) {
             Create your memories & forget who forget you
           </p>
           <div className="flex flex-row items-center justify-center gap-4">
-            <Login children={"Write your thought"} />
+            <Login>{"Write your thought"}</Login>
           </div>
         </div>
       )}
